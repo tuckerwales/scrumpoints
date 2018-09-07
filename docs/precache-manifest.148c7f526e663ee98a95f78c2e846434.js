fed1793047e06123bfce7b41bf5ba3657d20610b
Joshua Lee Tucker <josh@tucker.wales>
2018-09-07 21:46:17 +0100

    Deploy to GitHub pages.

---
+++ docs/precache-manifest.148c7f526e663ee98a95f78c2e846434.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "b2affd7d634aa0831f06",
+    "url": "/js/app.7ff8b66d.js"
+  },
+  {
+    "revision": "30604558efcf61c39d59d36d4f65212a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f32d150249392fbd7e03b746bbaf278e",
+    "url": "/img/logo.f32d1502.png"
+  },
+  {
+    "revision": "b2affd7d634aa0831f06",
+    "url": "/css/app.4433e955.css"
+  }
+];
